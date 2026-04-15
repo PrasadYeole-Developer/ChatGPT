483bdb4f3abfb454a3b309dc530421666611b815
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2026-04-15 10:40:21 +0530

    Setup: Socket.IO

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ const userModel = require("../Models/user.model");
 require("dotenv").config();
 
 const authUser = async (req, res, next) => {
-  const { token } = res.cookies;
+  const { token } = req.cookies;
   if (!token) {
     return res.status(401).json({
       message: "Unauthorized",
@@ -12,10 +12,13 @@ const authUser = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await userModel.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
     req.user = user;
     next();
   } catch (error) {
-    return res.status(500).json({ message: "Interval server error" });
+    return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
 
