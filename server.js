40f4ac8a0a08556d65b092029cae937ca69d9607
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2026-04-15 16:32:23 +0530

    (auth): Added a middleware at SocketIO connection function to secure connection to only loggen in users

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const app = require("./src/app");
 const connectToDB = require("./src/DB/db");
+const initSocketServer = require("./src/sockets/socket.server");
+const httpServer = require("http").createServer(app);
 
 connectToDB();
+initSocketServer(httpServer);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server started running on ${process.env.PORT}`);
+httpServer.listen(process.env.PORT, () => {
+  console.log(`Server started running on ${process.env.PORT}`);
 });
--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,7 +1,26 @@
 const { Server } = require("socket.io");
+const { cookie } = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../Models/user.model");
+require("dotenv");
 
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
+
+  io.use(async (req, next) => {
+    const cookies = cookie.parse(socket.handshake.headers.token || "");
+    if (!cookies.token) {
+      return new Error("Authentication error: No token provided");
+    }
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+      const user = await userModel.fineById(decoded.id);
+      socket.user = user;
+      next();
+    } catch (error) {
+      console.log("Authenticatio error: ", error);
+    }
+  });
 
   io.on("connection", (socket) => {
     console.log("New socket connection: ", socket.id);
