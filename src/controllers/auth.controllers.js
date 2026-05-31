3e3057564952cdede5708a140047f96fd67ed757
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2026-06-01 00:28:58 +0530

    feat(auth.controller.js): add logoutUser controller

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -66,4 +66,18 @@ const loginUser = async (req, res) => {
   });
 };
 
-module.exports = { registerUser, loginUser };
+const logoutUser = async (req, res) => {
+  try {
+    res.clearCookie("token");
+
+    return res.status(200).json({
+      message: "User logged out successfully.",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Logout failed",
+    });
+  }
+};
+
+module.exports = { registerUser, loginUser, logoutUser };
