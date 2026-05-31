d929680ca6fc28b5ca83c22d28e1c802cf7c25d4
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2026-05-31 17:02:11 +0530

    feat: Added Cors

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes");
 const chatRoutes = require("./routes/chat.routes");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:5173', // local frontend URL
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
