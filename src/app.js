959c919101f191fba082af2ce0fda802e3df2010
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2026-05-31 23:09:25 +0530

    update: app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes");
 const chatRoutes = require("./routes/chat.routes");
+const messageRoutes = require("./routes/message.routes");
 const cors = require("cors");
 
 const app = express();
@@ -14,5 +15,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/messages", messageRoutes);
 
 module.exports = app;
