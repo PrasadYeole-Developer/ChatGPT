9b668985da4f03bad41835a8af549f6a6471b321
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2026-06-01 00:31:29 +0530

    feat: added routes for logout

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authControllers = require("../controllers/auth.controllers");
 
 router.post("/register", authControllers.registerUser);
-router.post("/login", authControllers.loginUser)
+router.post("/login", authControllers.loginUser);
+router.post("/logout", authControllers.logoutUser);
 
 module.exports = router;
